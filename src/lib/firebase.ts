77f30ff91dc1ddf3fbd8704888652f32a02c0e21
Rishab Acharjee <rishab.acharjee12345@gmail.com>
2025-07-13 20:32:50 +0000

    see this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,17 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// IMPORTANT: This is a public sample configuration.
+// Replace with your own project's configuration.
 const firebaseConfig = {
-  apiKey: "AIzaSyBlxWnsCc4D2IPdGkLK1980sBGV3Ll9iR",
+  apiKey: "AIzaSyC0cE4aC0g8f2e4b6d8c0e4f2g6h2j4k2l",
   authDomain: "supplychainai-cdqvg.firebaseapp.com",
   projectId: "supplychainai-cdqvg",
   storageBucket: "supplychainai-cdqvg.appspot.com",
   messagingSenderId: "348749817829",
   appId: "1:348749817829:web:14cc068ab6dea01527310c"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
