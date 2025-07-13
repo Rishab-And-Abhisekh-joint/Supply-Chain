b7a3b545a1ba50c5640a4415b03225d33242a8fb
Rishab Acharjee <rishab.acharjee12345@gmail.com>
2025-07-13 20:31:48 +0000

    same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBlxWnsCc4D2IPdGkLK1980sBGV3Ll9iR",
+  authDomain: "supplychainai-cdqvg.firebaseapp.com",
+  projectId: "supplychainai-cdqvg",
+  storageBucket: "supplychainai-cdqvg.appspot.com",
+  messagingSenderId: "348749817829",
+  appId: "1:348749817829:web:14cc068ab6dea01527310c"
 };
 
 // Initialize Firebase
